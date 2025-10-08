5096459d4305ba55d3c788039bb3088b1515520e
Kishan20 <160807245+kishu20112002@users.noreply.github.com>
2025-10-08 17:04:55 +0530

    Add files via upload

---
+++ empty.py
@@ -0,0 +1,12 @@
+v=set()
+print(v,type(v))
+# output set() <class 'set'>
+t=()
+print(t,type(t))
+#output () <class 'tuple'>
+l=[]
+print(l,type(l))
+#output [] <class 'list'>
+d={}
+print(d,type(d))
+#output {} <class 'dict'>
